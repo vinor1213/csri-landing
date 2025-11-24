7d2d4d3c05676a47622771ff7e35c93a7c9a72b2
vinoth <vinor1213@gmail.com>
2025-11-24 12:39:45 +0530

    last psuh

--- data/newsandevents.ts
+++ data/newsandevents.ts
@@ -46,8 +46,8 @@ export const newsandeventsData: Record<string, EventType[]> = {
             endDate: "2025-11-19",
             description:
                 "The Inauguration Ceremony of the SRCL Self Employed Tailor programme proudly launches a transformative skill development initiative designed to uplift aspiring entrepreneurs. This event showcases SRCL’s commitment to empowering women and youth with professional tailoring expertise that opens doors to income generation and self-employment. With insightful addresses from dignitaries and a strong focus on community impact, the ceremony inspires participants to embark confidently on their journey toward financial independence and creative enterprise.",
-            imgSrc: "/images/newsandevents/1.jpg",
-            images: ["/images/newsandevents/1.jpg"],
+            imgSrc: "/images/newsandevents/news-1.webp",
+            images: ["/images/newsandevents/news-1.webp"],
             category: "Skill Development",
         },
         {
@@ -57,8 +57,8 @@ export const newsandeventsData: Record<string, EventType[]> = {
             endDate: "2025-11-17",
             description:
                 "The prestigious launch of “Sona Health on Wheels,” graced by the Hon’ble Tourism Minister, marks a visionary step in delivering holistic healthcare directly to communities. This state-of-the-art mobile wellness unit brings naturopathy, yoga therapy, lifestyle counselling, and health awareness to underserved regions. This pioneering effort integrates traditional healing with modern outreach, transforming rural wellness.",
-            imgSrc: "/images/newsandevents/6.jpg",
-            images: ["/images/newsandevents/6.jpg"],
+           imgSrc: "/images/newsandevents/news-2.webp",
+            images: ["/images/newsandevents/news-2.webp"],
             category: "Health & Well-being",
         },
     ],
@@ -71,8 +71,8 @@ export const newsandeventsData: Record<string, EventType[]> = {
             endDate: "",
             description:
                 "The Valedictory Ceremony celebrates the outstanding achievements of participants who have successfully completed the Self-Supportive Courses in Aari Work and Beauty Therapy. This vibrant event highlights their creativity, talent, and dedication to mastering high-demand skills.",
-            imgSrc: "/images/newsandevents/2.jpg",
-            images: ["/images/newsandevents/2.jpg"],
+            imgSrc: "/images/gallery/Fashion-Apparels/fashion-apparels-11.webp",
+            images: ["/images/gallery/Fashion-Apparels/fashion-apparels-11.webp"],
             category: "Skill Development",
         },
         {
@@ -82,8 +82,8 @@ export const newsandeventsData: Record<string, EventType[]> = {
             endDate: "",
             description:
                 "This celebratory ceremony honors the newly skilled graduates of the Self Employed Tailor and Domestic Data Entry Operator programmes, showcasing their readiness to step into workspaces or begin self-driven careers.",
-            imgSrc: "/images/newsandevents/3.jpg",
-            images: ["/images/newsandevents/3.jpg"],
+            imgSrc: "/images/gallery/Fashion-Apparels/fashion-apparels-21.webp",
+            images: ["/images/gallery/Fashion-Apparels/fashion-apparels-21.webp"],
             category: "Skill Development",
         },
         {
@@ -93,8 +93,8 @@ export const newsandeventsData: Record<string, EventType[]> = {
             endDate: "",
             description:
                 "The Certificate Distribution Ceremony brings together achievers from seven diverse Skill Development Programmes, celebrating their commitment to building future-ready careers and empowering individuals with industry-relevant expertise.",
-            imgSrc: "/images/newsandevents/4.jpg",
-            images: ["/images/newsandevents/4.jpg"],
+            imgSrc: "/images/gallery/Fashion-Apparels/fashion-apparels-20.webp",
+            images: ["/images/gallery/Fashion-Apparels/fashion-apparels-20.webp"],
             category: "Skill Development",
         },
         {
@@ -104,8 +104,8 @@ export const newsandeventsData: Record<string, EventType[]> = {
             endDate: "2025-11-21",
             description:
                 "The Faculty Development Program on “AI in Naturopathy and AYUSH” presents an exciting platform for educators to explore the future of traditional healthcare enhanced by cutting-edge technologies.",
-            imgSrc: "/images/newsandevents/5.jpg",
-            images: ["/images/newsandevents/5.jpg"],
+            imgSrc: "/images/gallery/health/health1-12.webp",
+            images: ["/images/gallery/health/health1-12.webp"],
             category: "Health & Well-being",
         },
         {
@@ -115,8 +115,8 @@ export const newsandeventsData: Record<string, EventType[]> = {
             endDate: "2025-02-22",
             description:
                 "The Naturopathy Medical Camp encourages students to adopt healthy, natural, preventive wellness practices through yoga sessions, nutrition guidance, health screenings and fun learning.",
-            imgSrc: "/images/newsandevents/7.jpg",
-            images: ["/images/newsandevents/7.jpg"],
+            imgSrc: "/images/gallery/health/health1-15.webp",
+            images: ["/images/gallery/health/health1-15.webp"],
             category: "Health & Well-being",
         },
         {
@@ -126,8 +126,8 @@ export const newsandeventsData: Record<string, EventType[]> = {
             endDate: "2025-02-25",
             description:
                 "The Career Guidance Program empowers students by introducing emerging career trends, in-demand skills, scholarship routes, and industry expectations.",
-            imgSrc: "/images/newsandevents/8.jpg",
-            images: ["/images/newsandevents/8.jpg"],
+            imgSrc: "/images/gallery/Fashion-Apparels/fashion-apparels-26.webp",
+            images: ["/images/gallery/Fashion-Apparels/fashion-apparels-26.webp"],
             category: "Skill Development",
         },
     ],
